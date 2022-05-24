ce0cafc6a3d09b5d31e8ba2ce110d76d9702658d
Leo <leusbst@googlemail.com>
2022-05-24 21:07:03 +0200

    sql 3 done

--- SQL/blatt_3/blatt_3_loesung.sql
+++ SQL/blatt_3/blatt_3_loesung.sql
@@ -1,27 +1,32 @@
-/*%%%%head%%{"type":"submission","name":"blatt3","authors":[["Leo Steiner","s3819219@stud.uni-frankfurt.de","7906565"]]}%%*/
+	/*%%%%head%%{"type":"submission","name":"blatt3","authors":[["Leo Steiner","s3819219@stud.uni-frankfurt.de","7906565"]]}%%*/
 /*%%1a%%*/
-/* a) */ select name, produzent, preis from medikament where name like 'Anti%'
+/* a) */ 
+select name, produzent, preis from medikament where name like 'Anti%'
 /* where binary for case sensitivity */
 
 /*%%1b%%*/
-/* b) */ select pnr, name from mitarbeiter where trim(name) like '_% _% %_';
+/* b) */ 
+select pnr, name from mitarbeiter where trim(name) like '_% _% %_';
 /* trim removes leading and trailing spaces */
 /* _% makes sure that there is at least on character */
 
 
 /*%%1c%%*/
-/* c) */ select produzent as  Hersteller, count(produzent) as Anzahl, ceil(avg (preis)) as Durchschnittspreis from medikament group by produzent order by Anzahl desc, Durchschnittspreis desc;
+/* c) */ 
+select produzent as  Hersteller, count(produzent) as Anzahl, ceil(avg (preis)) as Durchschnittspreis from medikament group by produzent order by Anzahl desc, Durchschnittspreis desc;
 /* slicker:  count(*) instead of count(Produzent) etc...*/
 
 
 /*%%1d%%*/
-/* d) */ select fachgebiet, count(fachgebiet) from arzt, mitarbeiter where arzt.pnr = mitarbeiter.pnr and mitarbeiter.gebdat >'1970-01-01' group by arzt.fachgebiet having count(fachgebiet)>1;
+/* d) */ 
+select fachgebiet, count(fachgebiet) from arzt, mitarbeiter where arzt.pnr = mitarbeiter.pnr and mitarbeiter.gebdat >'1970-01-01' group by arzt.fachgebiet having count(fachgebiet)>1;
 /* slicker:  just select fachgebiet, count(*) etc...*/
 
 
 
 /*%%1e%%*/
-/* e) */ select eingwpatient.patnr, patient.name, eingwpatient.stationid, station.name from eingwpatient, patient, station where eingwpatient.patnr = patient.patnr and eingwpatient.stationid = station.stationid and station.stationid <3 order by station.stationid desc, eingwpatient.patnr asc;
+/* e) */ 
+select eingwpatient.patnr, patient.name, eingwpatient.stationid, station.name from eingwpatient, patient, station where eingwpatient.patnr = patient.patnr and eingwpatient.stationid = station.stationid and station.stationid <3 order by station.stationid desc, eingwpatient.patnr asc;
 
 
 
@@ -33,5 +38,7 @@ select eingwpatient.patnr, patient.name, eingwpatient.stationid, station.name, e
 
 
 /*%%1g%%*/
-/* g) */ select name, gebdat, pnr  from mitarbeiter where date_format(gebdat, '%m-%d') in (select date_format(gebdat, '%m-%d') from mitarbeiter group by gebdat having count(gebdat)>2) order by pnr asc;
-/* needs to be fixed */
+/* g) */ 
+select m1.pnr, group_concat(m2.pnr order by m2.pnr) as gleicherGeburtstagWie from mitarbeiter m1, mitarbeiter m2 where (month(m1.gebdat) = month(m2.gebdat) and dayofmonth(m1.gebdat) = dayofmonth(m2.gebdat)) and m1.pnr <> m2.pnr group by m1.pnr having count(*) > 1 order by m1.pnr;
+/* */
+
